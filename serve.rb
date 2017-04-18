c44f567a4ab77a5c19a002a450e5a5fc66557b02
Joscha Feth <joscha@feth.com>
2017-04-18 15:16:44 +1000

    Don't log

--- serve.rb
+++ serve.rb
@@ -1,6 +1,10 @@
 require 'webrick'
 
-server = WEBrick::HTTPServer.new(Port: 9099, DocumentRoot: File.expand_path('./storybook-static', __dir__))
+server = WEBrick::HTTPServer.new(
+  Port: 9099,
+  DocumentRoot: File.expand_path('./storybook-static', __dir__),
+  AccessLog: [], # Don't log visited resources in the Percy run
+)
 trap 'INT' do
   server.shutdown
 end
